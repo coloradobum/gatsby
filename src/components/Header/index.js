4834bf6fcb81a8cc10a497ace02ce30935bd4855
Jeff Thomas <subskriptions@gmail.com>
2018-03-11 08:57:22 -0600

    styling not right on header.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+
 import logo from '../../images/logo.svg'
 
-console.log(logo)
+const HeaderWrapper = styled.div`
+  background: #524763;
+  marginBottom: 1.5rem;
+  h1 {
+    img {
+      height: 80px;
+    }
+  }
+`;
+
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  maxWidth: 960px;
+  padding: 1.45rem 1.0875rem;
+`;
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
+  <HeaderWrapper>
+    <HeaderContainer>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -29,26 +34,26 @@ const Header = () => (
           <img src={logo} alt='Level Up Logo' />
         </Link>
       </h1>
-    </div>
-    <nav>
-      <ul>
-        <li>
-          <Link
-            to='/'
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            to='/about'
-          >
-            About
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  </div>
+      <nav>
+        <ul>
+          <li>
+            <Link
+              to='/'
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to='/about'
+            >
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </HeaderContainer>
+  </HeaderWrapper>
 )
 
 export default Header
