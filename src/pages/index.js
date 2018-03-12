172784fe549baf249a60e210c282a2391e40d332
Jeff Thomas <subskriptions@gmail.com>
2018-03-12 17:21:37 -0600

    End of 9.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,15 @@ import Img from 'gatsby-image'
 
 const IndexPage = ({data}) => (
   <div>
-    <h1>Hello people</h1>
     <p>{data.site.siteMetadata.title}</p>
     <p>{data.site.siteMetadata.desc}</p>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    {data.allMarkdownRemark.edges.map(({node}) => {
+      return <PostListing post={node} />
+    })}
   </div>
 )
+
+const PostListing = () => <div>hello</div>
 
 export default IndexPage
 
@@ -20,6 +22,17 @@ query MyQueryName {
     siteMetadata {
       title
       desc
+    }
+  }
+  allMarkdownRemark {
+    edges {
+      node {
+        frontmatter {
+          title
+          date(formatString: "MM-DD-YYYY")
+        }
+        html
+      }
     }
   }
 }
---
+++ src/pages/post.md
@@ -0,0 +1,8 @@
+---
+title: 'welcome to the new blog'
+date: '2018-03-12'
+---
+
+# Hello
+
+This is a new post
