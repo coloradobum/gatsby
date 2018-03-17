4204bd1ea292e985cf18f83fa01d4a14b5fb71b5
Jeff Thomas <subskriptions@gmail.com>
2018-03-17 16:07:24 -0600

    End of 10.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Img from 'gatsby-image'
+import PostListing from '../components/Posts/PostListing'
 
 const IndexPage = ({data}) => (
   <div>
-    <p>{data.site.siteMetadata.title}</p>
-    <p>{data.site.siteMetadata.desc}</p>
-    {data.allMarkdownRemark.edges.map(({node}) => {
-      return <PostListing post={node} />
-    })}
+    <h2>Posts</h2>
+    {data.allMarkdownRemark.edges.map(({node}) => (
+      <PostListing key={node.id} post={node} />
+    ))}
   </div>
 )
-
-const PostListing = () => <div>hello</div>
 
 export default IndexPage
 
@@ -27,11 +25,13 @@ query MyQueryName {
   allMarkdownRemark {
     edges {
       node {
+        id
         frontmatter {
           title
           date(formatString: "MM-DD-YYYY")
         }
         html
+        excerpt
       }
     }
   }
--- src/pages/post.md
+++
@@ -1,8 +0,0 @@
----
-title: 'welcome to the new blog'
-date: '2018-03-12'
----
-
-# Hello
-
-This is a new post
