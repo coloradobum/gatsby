14280ca172362e6dfbffb00fa41510b392c113f3
Jeff Thomas <subskriptions@gmail.com>
2018-03-28 10:15:46 -0600

    end of 14.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,41 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import Img from 'gatsby-image'
-import PostListing from '../components/Posts/PostListing'
+import React from "react";
+import Link from "gatsby-link";
+import PostListing from "../components/Posts/PostListing";
 
-const IndexPage = ({data}) => (
+const IndexPage = ({ data }) => (
   <div>
     <h2>Posts</h2>
-    {data.allMarkdownRemark.edges.map(({node}) => (
+    {data.allMarkdownRemark.edges.map(({ node }) => (
       <PostListing key={node.id} post={node} />
     ))}
   </div>
-)
+);
 
-export default IndexPage
+export default IndexPage;
 
 export const query = graphql`
-query MyQueryName {
-  site {
-    siteMetadata {
-      title
-      desc
+  query SiteMeta {
+    site {
+      siteMetadata {
+        title
+        desc
+      }
     }
-  }
-  allMarkdownRemark( sort: {
-    fields: [frontmatter__date],
-    order: DESC
-  }) {
-    edges {
-      node {
-        id
-        frontmatter {
-          title
-          date(formatString: "MM-DD-YYYY")
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD YYYY")
+          }
+          fields {
+            slug
+          }
+          html
+          excerpt
         }
-        fields {
-          slug
-        }
-        html excerpt
       }
     }
   }
-}
-`
+`;
